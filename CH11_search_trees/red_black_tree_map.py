90c42b51302c41b3c45cc1707f75700781142764
Ben <bdye71@gmail.com>
2020-10-13 19:04:27 -0400

    initial setup for red black tree

---
+++ CH11_search_trees/red_black_tree_map.py
@@ -0,0 +1,4 @@
+from tree_map import TreeMap
+
+class RedBlackTreeMap(TreeMap):
+    """Sorted map implementation using a red-black tree."""
---
+++ CH11_search_trees/test_red_black_tree_map.py
@@ -0,0 +1,6 @@
+import unittest
+
+from red_black_tree_map import RedBlackTreeMap
+
+if __name__ == '__main__':
+    unittest.main()
